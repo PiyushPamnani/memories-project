1e027f1666acbe4c801d8804964f2bbbf3020089
Piyush Pamnani <piyushpamnani46@gmail.com>
2023-04-11 23:22:43 +0530

    Render-Link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
-// const url = "https://memories-project-2j9t.onrender.com/posts";
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://memories-project-2j9t.onrender.com",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
